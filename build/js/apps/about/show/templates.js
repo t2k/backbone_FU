96614703efa65ca78992f44366216c896dc7de45
Ted Killilea Win7_NYC <lrrs259@gmail.com>
2014-03-20 17:19:18 -0400

    .gitignore now working

--- build/js/apps/about/show/templates.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  define(function(require) {
-    return {
-      layout: require("text!apps/about/show/templates/layout.htm"),
-      about: require("text!apps/about/show/templates/about.htm")
-    };
-  });
-
-}).call(this);
